e85f2c4f74aee86de1da219693d22537b648618c
Joseph Yeo <joseph@hypezero.com>
2020-11-12 20:43:20 +0000

    Com[leted exercise

--- DevInt.Core.Tests/Calculators/StringCalculatorTests.cs
+++ DevInt.Core.Tests/Calculators/StringCalculatorTests.cs
@@ -12,9 +12,12 @@ namespace DevInt.Core.Calculators
         string twoNumbers = "11,54";
         string multiple = "9,5,34,7,89,3,6,54";
         string validNewLines = "1\n2,3";
-        string invalidNewLines = "1,\n";
-        string validNewLines2 = "9,5,34,7\n89,3\n6,54";
+        string validNewLines2 = "9,5,34,7\n89,3\n6,54"; 
+        string invalidNewLines = "1,\n";        
         string customDelimited = "//;\n1;2";
+        string negativeNumbers = "1,1,2,3,5,8,-8,13,21,-26,9001,34";
+        string numbersOver1000 = "2,1001,13";
+        string multipleDelimiters = "//*%\n1*2%3";
 
         [Fact]
         public void C1CanHandleEmptyString()
@@ -73,7 +76,7 @@ namespace DevInt.Core.Calculators
         }
 
         [Fact]
-        public void C3CanHandleNewLines()
+        public void C3CanHandleNewLineCharacters()
         {
             //Arrange
             var calculator = new StringCalculator();
@@ -85,7 +88,6 @@ namespace DevInt.Core.Calculators
             var resultValidNewLines = calculator.C3Add(validNewLines);
             var resultValidNewLine2 = calculator.C3Add(validNewLines2);
             
-
             //Assert
             Assert.Equal(0, resultEmpty);
             Assert.Equal(7, resultOne);
@@ -97,18 +99,46 @@ namespace DevInt.Core.Calculators
         }
 
         [Fact]
-        public void C4CanAdd()
+        public void C4CanSupportDifferentDelimiters()
         {
             //Arrange
             var calculator = new StringCalculator();
 
             //Act
-            var resultEmpty = calculator.Add(emptyString);
-            var resultOne = calculator.Add(oneNumber);
-            var resultMultiple = calculator.Add(multiple);
-            var resultValidNewLines = calculator.Add(validNewLines);
-            var resultValidNewLine2 = calculator.Add(validNewLines2);
-            var resultCustomDelimited = calculator.Add(customDelimited);
+            var resultEmpty = calculator.C4Add(emptyString);
+            var resultOne = calculator.C4Add(oneNumber);
+            var resultMultiple = calculator.C4Add(multiple);
+            var resultValidNewLines = calculator.C4Add(validNewLines);
+            var resultValidNewLine2 = calculator.C4Add(validNewLines2);
+            var resultCustomDelimited = calculator.C4Add(customDelimited);
+            
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+            Assert.Equal(6, resultValidNewLines);
+            Assert.Equal(207, resultValidNewLine2);
+            //Asserting that an exception is thrown if input is incorrect, though
+            //more details on the exception are not instrumented for this particular assertion
+            Assert.Throws<ArgumentException>(() => calculator.C4Add(invalidNewLines));
+            Assert.Equal(3, resultCustomDelimited);
+            
+        }
+
+        [Fact]
+        public void C5CanHandleNegativeValues()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var resultEmpty = calculator.C5Add(emptyString);
+            var resultOne = calculator.C5Add(oneNumber);
+            var resultMultiple = calculator.C5Add(multiple);
+            var resultValidNewLines = calculator.C5Add(validNewLines);
+            var resultValidNewLine2 = calculator.C5Add(validNewLines2);
+            var resultCustomDelimited = calculator.C5Add(customDelimited);
+            
 
             //Assert
             Assert.Equal(0, resultEmpty);
@@ -116,8 +146,108 @@ namespace DevInt.Core.Calculators
             Assert.Equal(207, resultMultiple);
             Assert.Equal(6, resultValidNewLines);
             Assert.Equal(207, resultValidNewLine2);
-            Assert.Throws<ArgumentException>(() => calculator.Add(invalidNewLines));
+            //Asserting that an exception is thrown if input is incorrect, though
+            //more details on the exception are not instrumented for this particular assertion
+            Assert.Throws<ArgumentException>(() => calculator.C5Add(invalidNewLines));
             Assert.Equal(3, resultCustomDelimited);
+            
+            //Could use a standard ArgumentException here but we try to be more specific
+            //and indicate range is the issue here for negative input
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => calculator.C5Add(negativeNumbers));
+            Assert.Equal("source", exception.ParamName);
+            Assert.StartsWith("Negatives not allowed", exception.Message);
+            //As per comments in StringCalculator, we test here to make sure we are sending back
+            //a valid list of negative numbers in the Data dictionary
+            var lstNegatives = (List<int>)exception.Data["negatives"];
+            Assert.Equal(2, lstNegatives.Count); //do we have 2 negative numbers?
+            Assert.Equal(-26, lstNegatives[1]); //are they the ones in the test string?
+
+        }
+
+        [Fact]
+        public void C6CanHandleNumbersOver1000()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var resultEmpty = calculator.C6Add(emptyString);
+            var resultOne = calculator.C6Add(oneNumber);
+            var resultMultiple = calculator.C6Add(multiple);
+            var resultValidNewLines = calculator.C6Add(validNewLines);
+            var resultValidNewLine2 = calculator.C6Add(validNewLines2);
+            var resultCustomDelimited = calculator.C6Add(customDelimited);
+            var resultNumbersOver1000 = calculator.C6Add(numbersOver1000);
+
+
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+            Assert.Equal(6, resultValidNewLines);
+            Assert.Equal(207, resultValidNewLine2);
+            //Asserting that an exception is thrown if input is incorrect, though
+            //more details on the exception are not instrumented for this particular assertion
+            Assert.Throws<ArgumentException>(() => calculator.C6Add(invalidNewLines));
+            Assert.Equal(3, resultCustomDelimited);
+
+            //Could use a standard ArgumentException here but we try to be more specific
+            //and indicate range is the issue here for negative input
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => calculator.C6Add(negativeNumbers));
+            Assert.Equal("source", exception.ParamName);
+            Assert.StartsWith("Negatives not allowed", exception.Message);
+            //As per comments in StringCalculator, we test here to make sure we are sending back
+            //a valid list of negative numbers in the Data dictionary
+            var lstNegatives = (List<int>)exception.Data["negatives"];
+            Assert.Equal(2, lstNegatives.Count); //do we have 2 negative numbers?
+            Assert.Equal(-26, lstNegatives[1]); //are they the ones in the test string?
+
+            Assert.Equal(15, resultNumbersOver1000);
+        }
+
+        [Fact]
+        public void C7CanHandleMultipleDelimiters()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var resultEmpty = calculator.C7Add(emptyString);
+            var resultOne = calculator.C7Add(oneNumber);
+            var resultMultiple = calculator.C7Add(multiple);
+            var resultValidNewLines = calculator.C7Add(validNewLines);
+            var resultValidNewLine2 = calculator.C7Add(validNewLines2);
+            var resultCustomDelimited = calculator.C7Add(customDelimited);
+            var resultNumbersOver1000 = calculator.C7Add(numbersOver1000);
+            var resultmultipleDelimiters = calculator.C7Add(multipleDelimiters);
+
+            
+
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+            Assert.Equal(6, resultValidNewLines);
+            Assert.Equal(207, resultValidNewLine2);
+            //Asserting that an exception is thrown if input is incorrect, though
+            //more details on the exception are not instrumented for this particular assertion
+            Assert.Throws<ArgumentException>(() => calculator.C7Add(invalidNewLines));
+            Assert.Equal(3, resultCustomDelimited);
+
+            //Could use a standard ArgumentException here but we try to be more specific
+            //and indicate range is the issue here for negative input
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => calculator.C7Add(negativeNumbers));
+            Assert.Equal("source", exception.ParamName);
+            Assert.StartsWith("Negatives not allowed", exception.Message);
+            //As per comments in StringCalculator, we test here to make sure we are sending back
+            //a valid list of negative numbers in the Data dictionary
+            var lstNegatives = (List<int>)exception.Data["negatives"];
+            Assert.Equal(2, lstNegatives.Count); //do we have 2 negative numbers?
+            Assert.Equal(-26, lstNegatives[1]); //are they the ones in the test string?
+
+            Assert.Equal(15, resultNumbersOver1000);
+            Assert.Equal(6, resultmultipleDelimiters);
+
         }
 
     }
--- DevInt.Core/Calculators/StringCalculator.cs
+++ DevInt.Core/Calculators/StringCalculator.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DevInt.Core.Calculators
 {
@@ -14,27 +18,19 @@ namespace DevInt.Core.Calculators
         public int C1Add(string source)
         {
             var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
 
             var lstNumbers = source.Split(",");
 
-            switch(lstNumbers.Length)
+            if(lstNumbers.Length == 1)
             {
-                case 0:
-                    calcValue= 0;
-                    break;
-
-                case 1:
-                    Int32.TryParse(lstNumbers[0], out calcValue);
-                    break;
-
-                case 2:
-                    Int32.TryParse(lstNumbers[0], out int first);
-                    Int32.TryParse(lstNumbers[1], out int second);
-                    calcValue = first + second;
-                    break;
-
-                default:
-                    break;
+                Int32.TryParse(lstNumbers[0], out calcValue);
+            }
+            else
+            {
+                Int32.TryParse(lstNumbers[0], out int first);
+                Int32.TryParse(lstNumbers[1], out int second);
+                calcValue = first + second;
             }
 
             return calcValue;
@@ -44,6 +40,7 @@ namespace DevInt.Core.Calculators
         public int C2Add(string source)
         {
             var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
 
             var lstNumbers = source.Split(",");
 
@@ -60,9 +57,12 @@ namespace DevInt.Core.Calculators
         public int C3Add(string source)
         {
             var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
 
             //First clean up input string
             source = source.Replace("\n", ",");
+
+            //and get the list if numbers
             var lstNumbers = source.Split(",");
 
             //For invalid new line character, according to example,
@@ -71,7 +71,7 @@ namespace DevInt.Core.Calculators
             var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
             if(emptyExists)
             {
-                throw new ArgumentException("Invalid input string");
+                throw new ArgumentException("Invalid input string", "source");
             }
 
             foreach (var item in lstNumbers)
@@ -84,23 +84,27 @@ namespace DevInt.Core.Calculators
 
         }
 
-        public int Add(string source)
+        public int C4Add(string source)
         {
             var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
+
             var delimiter = ",";
 
-            //First see if there is a custom delimiter
-            if(source.Length > 5)
+            //First see if there is a custom delimiter in a header record
+            if (source.Length > 5)
             {
-                if((source.Substring(0,2) == "//") && (source.Substring(3,1) == "\n"))
-                 {
+                if ((source.Substring(0, 2) == "//") && (source.Substring(3, 1) == "\n"))
+                {
                     delimiter = source.Substring(2, 1);
-                    source = source.Substring(4); //remove the header
+                    source = source.Substring(4); //remove the header with the delimiter
                 }
             }
 
-            //First clean up input string
+            //Next clean up input string
             source = source.Replace("\n", delimiter);
+
+            //and get the list of numbers
             var lstNumbers = source.Split(delimiter);
 
             //For invalid new line character, according to example,
@@ -109,7 +113,7 @@ namespace DevInt.Core.Calculators
             var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
             if (emptyExists)
             {
-                throw new ArgumentException("Invalid input string");
+                throw new ArgumentException("Invalid input string", "source");
             }
 
             foreach (var item in lstNumbers)
@@ -117,6 +121,237 @@ namespace DevInt.Core.Calculators
                 Int32.TryParse(item, out int number);
                 calcValue += number;
             }
+
+            return calcValue;
+
+        }
+            
+        public int C5Add(string source)
+        {
+            var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
+
+            var delimiter = ",";
+
+            //First see if there is a custom delimiter in a header record
+            if (source.Length > 5)
+            {
+                if ((source.Substring(0, 2) == "//") && (source.Substring(3, 1) == "\n"))
+                {
+                    delimiter = source.Substring(2, 1);
+                    source = source.Substring(4); //remove the header with the delimiter
+                }
+            }
+
+            //Next clean up input string
+            source = source.Replace("\n", delimiter);
+
+            //and get the list if numbers
+            var lstNumbers = source.Split(delimiter);
+
+            //For invalid new line character, according to example,
+            //we will end up with number items which are empty strings
+            //so check for empty string but only if more than 1 number
+            var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
+            if (emptyExists)
+            {
+                throw new ArgumentException("Invalid input string", "source");
+            }
+
+            // To deal with multiple possible negative values, and report them, it will
+            // be easier to transpose the list to one of integers and operate on that instead
+            var lstIntegers = new List<Int32>();
+            foreach (var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                lstIntegers.Add(number);
+            }
+
+            //Get list of negative integers, if any
+            var lstNegativeIntegers = (from ent in lstIntegers
+                                       where (ent < 0)
+                                       select ent).ToList();
+
+            if (lstNegativeIntegers.Count > 0)
+            {
+                var concatNegatives = string.Join(",", lstNegativeIntegers);
+                var message = $"Negatives not allowed. Found:{concatNegatives}";
+                
+                var exception = new ArgumentOutOfRangeException("source", message);
+
+                //That exception message works as requested but could be more helpful if we provided
+                //the list of negative numbers in a more useable format. It could be further processed 
+                //or analysed by Serilog for example
+                exception.Data["negatives"] = lstNegativeIntegers;
+                throw exception;
+            }
+
+            calcValue = lstIntegers.Sum();
+
+            return calcValue;
+
+        }
+
+        public int C6Add(string source)
+        {
+            var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
+
+            var delimiter = ",";
+
+            //First see if there is a custom delimiter in a header record
+            if (source.Length > 5)
+            {
+                if ((source.Substring(0, 2) == "//") && (source.Substring(3, 1) == "\n"))
+                {
+                    delimiter = source.Substring(2, 1);
+                    source = source.Substring(4); //remove the header with the delimiter
+                }
+            }
+
+            //Next clean up input string
+            source = source.Replace("\n", delimiter);
+
+            //and get the list if numbers
+            var lstNumbers = source.Split(delimiter);
+
+            //For invalid new line character, according to example,
+            //we will end up with number items which are empty strings
+            //so check for empty string but only if more than 1 number
+            var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
+            if (emptyExists)
+            {
+                throw new ArgumentException("Invalid input string", "source");
+            }
+
+            // To deal with multiple possible negative values, and report them, it will
+            // be easier to transpose the list to one of integers and operate on that instead
+            var lstIntegers = new List<Int32>();
+            foreach (var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                lstIntegers.Add(number);
+            }
+
+            //Get list of negative integers, if any
+            var lstNegativeIntegers = (from ent in lstIntegers
+                                       where (ent < 0)
+                                       select ent).ToList();
+
+            if (lstNegativeIntegers.Count > 0)
+            {
+                var concatNegatives = string.Join(",", lstNegativeIntegers);
+                var message = $"Negatives not allowed. Found:{concatNegatives}";
+
+                var exception = new ArgumentOutOfRangeException("source", message);
+
+                //That exception message works as requested but could be more helpful if we provided
+                //the list of negative numbers in a more useable format. It could be further processed 
+                //or analysed by Serilog for example
+                exception.Data["negatives"] = lstNegativeIntegers;
+                throw exception;
+            }
+
+            //Simply remove of elements greater than 1000
+            lstIntegers = (from ent in lstIntegers
+                           where (ent <= 1000)
+                           select ent).ToList();
+
+            calcValue = lstIntegers.Sum();
+
+            return calcValue;
+
+        }
+
+        public int C7Add(string source)
+        {
+            var calcValue = 0;
+            if (string.IsNullOrEmpty(source)) return calcValue;
+
+            var delimiter = ","; //set the default delimiter
+
+            //we will process a string called payload. For now, set this to source as default
+            var payload = source;
+
+
+            //First see if there is a custom delimiter(s) in a header record
+            if ((source.Length >= 4) && (source.StartsWith("//"))) //Make sure the length is long enough to have a header record
+                                                                   //and we can do the following string manipulations
+            {
+                //If the source string begins with "//" and there is a line break, then consider
+                //these characters represent the delimiter record header and the characters in between
+                //are the delimiter characters
+                var startIndex = source.IndexOf("//"); //always == 0. Just added for clarity
+                var endIndex = source.IndexOf("\n");
+
+                var allDelimiters = source.Substring(2, (endIndex - 2));
+                //payload then is the source trng with the delimiter record header removed
+                payload = source.Substring(endIndex + 1);
+
+                //By definition, delimiter characters only represent delimiting positions (and not input numbers)
+                //so we can choose the first one as the new default and replace all the others with it
+                //First get the array of characters
+                var arrDelimiters = allDelimiters.ToCharArray();
+                delimiter = arrDelimiters[0].ToString();
+
+                for (int i = 1; i < arrDelimiters.Length; i++)
+                {
+                    //convert each char to a string
+                    var delim = arrDelimiters[i].ToString();
+                    payload = payload.Replace(delim, delimiter);
+                }
+
+            }
+
+            //Next clean up input string to remove any new line characters
+            payload = payload.Replace("\n", delimiter);
+
+            //and get the list if numbers
+            var lstNumbers = payload.Split(delimiter);
+
+            //For invalid new line character, according to example,
+            //we will end up with number items which are empty strings
+            //so check for empty string but only if more than 1 number
+            var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
+            if (emptyExists)
+            {
+                throw new ArgumentException("Invalid input string", "source");
+            }
+
+            // To deal with multiple possible negative values, and report them, it will
+            // be easier to transpose the list to one of integers and operate on that instead
+            var lstIntegers = new List<Int32>();
+            foreach (var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                lstIntegers.Add(number);
+            }
+
+            //Get list of negative integers, if any
+            var lstNegativeIntegers = (from ent in lstIntegers
+                                       where (ent < 0)
+                                       select ent).ToList();
+
+            if (lstNegativeIntegers.Count > 0)
+            {
+                var concatNegatives = string.Join(",", lstNegativeIntegers);
+                var message = $"Negatives not allowed. Found:{concatNegatives}";
+
+                var exception = new ArgumentOutOfRangeException("source", message);
+
+                //That exception message works as requested but could be more helpful if we provided
+                //the list of negative numbers in a more useable format. It could be further processed 
+                //or analysed by Serilog for example
+                exception.Data["negatives"] = lstNegativeIntegers;
+                throw exception;
+            }
+
+            //Simply remove of elements greater than 1000
+            lstIntegers = (from ent in lstIntegers
+                           where (ent <= 1000)
+                           select ent).ToList();
+
+            calcValue = lstIntegers.Sum();
 
             return calcValue;
 
