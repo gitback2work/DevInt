1b7023cbb31622d2b5f6f019f722b033e13d0da5
Joseph Yeo <joseph@hypezero.com>
2020-11-12 12:59:47 +0000

    Add project files.

---
+++ DevInt.Core.Tests/Calculators/StringCalculatorTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DevInt.Core.Calculators
+{
+    public class StringCalculatorTests
+    {
+        string emptyString = "";
+        string oneNumber = "7";
+        string twoNumbers = "11,54";
+        string multiple = "9,5,34,7,89,3,6,54";
+        string validNewLines = "1\n2,3";
+        string invalidNewLines = "1,\n";
+        string validNewLines2 = "9,5,34,7\n89,3\n6,54";
+        string customDelimited = "//;\n1;2";
+
+        [Fact]
+        public void C1CanHandleEmptyString()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var result = calculator.C1Add(emptyString);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void C1CanHandleOneNumber()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var result = calculator.C1Add(oneNumber);
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void C1CanHandleTwoNumbers()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var result = calculator.C1Add(twoNumbers);
+
+            //Assert
+            Assert.Equal(65, result);
+        }
+
+        [Fact]
+        public void C2CanHandleMultipleNumbers()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var resultEmpty = calculator.C2Add(emptyString);
+            var resultOne = calculator.C2Add(oneNumber);
+            var resultMultiple = calculator.C2Add(multiple);
+
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+        }
+
+        [Fact]
+        public void C3CanHandleNewLines()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act          
+            var resultEmpty = calculator.C3Add(emptyString);
+            var resultOne = calculator.C3Add(oneNumber);
+            var resultMultiple = calculator.C3Add(multiple);
+            var resultValidNewLines = calculator.C3Add(validNewLines);
+            var resultValidNewLine2 = calculator.C3Add(validNewLines2);
+            
+
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+            Assert.Equal(6, resultValidNewLines);
+            Assert.Equal(207, resultValidNewLine2);
+            Assert.Throws<ArgumentException>(() => calculator.C3Add(invalidNewLines));
+            
+        }
+
+        [Fact]
+        public void C4CanAdd()
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var resultEmpty = calculator.Add(emptyString);
+            var resultOne = calculator.Add(oneNumber);
+            var resultMultiple = calculator.Add(multiple);
+            var resultValidNewLines = calculator.Add(validNewLines);
+            var resultValidNewLine2 = calculator.Add(validNewLines2);
+            var resultCustomDelimited = calculator.Add(customDelimited);
+
+            //Assert
+            Assert.Equal(0, resultEmpty);
+            Assert.Equal(7, resultOne);
+            Assert.Equal(207, resultMultiple);
+            Assert.Equal(6, resultValidNewLines);
+            Assert.Equal(207, resultValidNewLine2);
+            Assert.Throws<ArgumentException>(() => calculator.Add(invalidNewLines));
+            Assert.Equal(3, resultCustomDelimited);
+        }
+
+    }
+}
---
+++ DevInt.Core/Calculators/StringCalculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+
+namespace DevInt.Core.Calculators
+{
+    // Assume all numbers in input string are integers as there is no instruction
+    //on how to convert to int32 result
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+        }
+
+        public int C1Add(string source)
+        {
+            var calcValue = 0;
+
+            var lstNumbers = source.Split(",");
+
+            switch(lstNumbers.Length)
+            {
+                case 0:
+                    calcValue= 0;
+                    break;
+
+                case 1:
+                    Int32.TryParse(lstNumbers[0], out calcValue);
+                    break;
+
+                case 2:
+                    Int32.TryParse(lstNumbers[0], out int first);
+                    Int32.TryParse(lstNumbers[1], out int second);
+                    calcValue = first + second;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return calcValue;
+
+        }
+
+        public int C2Add(string source)
+        {
+            var calcValue = 0;
+
+            var lstNumbers = source.Split(",");
+
+            foreach(var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                calcValue += number;
+            }
+
+            return calcValue;
+
+        }
+
+        public int C3Add(string source)
+        {
+            var calcValue = 0;
+
+            //First clean up input string
+            source = source.Replace("\n", ",");
+            var lstNumbers = source.Split(",");
+
+            //For invalid new line character, according to example,
+            //we will end up with number items which are empty strings
+            //so check for empty string but only if more than 1 number
+            var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
+            if(emptyExists)
+            {
+                throw new ArgumentException("Invalid input string");
+            }
+
+            foreach (var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                calcValue += number;
+            }
+
+            return calcValue;
+
+        }
+
+        public int Add(string source)
+        {
+            var calcValue = 0;
+            var delimiter = ",";
+
+            //First see if there is a custom delimiter
+            if(source.Length > 5)
+            {
+                if((source.Substring(0,2) == "//") && (source.Substring(3,1) == "\n"))
+                 {
+                    delimiter = source.Substring(2, 1);
+                    source = source.Substring(4); //remove the header
+                }
+            }
+
+            //First clean up input string
+            source = source.Replace("\n", delimiter);
+            var lstNumbers = source.Split(delimiter);
+
+            //For invalid new line character, according to example,
+            //we will end up with number items which are empty strings
+            //so check for empty string but only if more than 1 number
+            var emptyExists = (lstNumbers.Count() > 1) && lstNumbers.Contains("");
+            if (emptyExists)
+            {
+                throw new ArgumentException("Invalid input string");
+            }
+
+            foreach (var item in lstNumbers)
+            {
+                Int32.TryParse(item, out int number);
+                calcValue += number;
+            }
+
+            return calcValue;
+
+        }
+
+    }
+}
